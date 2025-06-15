5fd2011e43089243f9fc653f2cdbb54d138f763d
mike <mike.jini@gmail.com>
2025-06-15 22:42:27 +0300

    on everyones soul this works

--- movies/test/test.py
+++ movies/test/test.py
@@ -1,7 +1,12 @@
 import requests
+import time
 
 HOST_ADDRESS = "127.0.0.1"
 PORT = "5000"
+WAIT_TIME = 5
+
+# Let the API wake up
+time.sleep(WAIT_TIME)
 
 response = requests.get(f'http://{HOST_ADDRESS}:{PORT}/movie')
 
